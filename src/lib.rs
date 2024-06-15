1e4b0c1dee8991f5c7e18d2e16ff3c8ee4f8baf7
Marhead <clgns0415@gmail.com>
2024-06-15 20:00:23 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -17,24 +17,58 @@ pub fn web_editor(dom: &str) -> Result<(), JsValue> {
     editor.set_attribute("placeholder", "내용을 입력하세요."); //placeholder 속성이나 div에는 동작하지 않음. 추후 기능을 넣기 위해 넣은 요소
     editor.set_inner_html("<p><br></p>"); //개행(엔터) 처리가 될 때 p태그가 추가되게 하기 위하여 초기값으로 <p> 태그를 넣어준다.
 
+    /** 툴바 추가 **/
+    let toolbar = document.create_element("div").unwrap(); //툴바의 부모 div
+    toolbar.set_class_name("wasm-toolbar"); //css를 위해 클래스를 설정해줌.
+
+    let textleft = document.create_element("button").unwrap();     //document.createElement('button')
+    textleft.set_class_name("toolbar-menu text-align-left");                // textleft.className
+    textleft.set_inner_html(&"왼");                                         //textleft.setInnerHtml
+    toolbar.append_child(&textleft);                                        //toolbar.appendChild
+
+    let textleft = document.create_element("button").unwrap();
+    textleft.set_class_name("toolbar-menu text-align-left");
+    textleft.set_inner_html(&"왼");
+    toolbar.append_child(&textleft);
+    
+    let textcenter = document.create_element("button").unwrap();
+    textcenter.set_class_name("toolbar-menu text-align-center");
+    textcenter.set_inner_html(&"가");
+    toolbar.append_child(&textcenter);
+    
+    let textright = document.create_element("button").unwrap();
+    textright.set_class_name("toolbar-menu text-align-right");
+    textright.set_inner_html(&"오");
+    toolbar.append_child(&textright);
+    
+    let bold = document.create_element("button").unwrap();
+    bold.set_class_name("toolbar-menu bold");
+    bold.set_inner_html(&"B");
+    toolbar.append_child(&bold);
+    
+    let italic = document.create_element("button").unwrap();
+    italic.set_class_name("toolbar-menu italic");
+    italic.set_inner_html(&"I");
+    toolbar.append_child(&italic);
+    
+    let underline = document.create_element("button").unwrap();
+    underline.set_class_name("toolbar-menu underline");
+    underline.set_inner_html(&"U");
+    toolbar.append_child(&underline);
+    
+    for i in 1..6 {
+        let heading = document.create_element("button").unwrap();
+        heading.set_class_name("toolbar-menu heading");
+        heading.set_inner_html(&format!("H{}", i));
+        toolbar.append_child(&heading);
+    }
+    
+    let hyperlink = document.create_element("button").unwrap();
+    hyperlink.set_class_name("toolbar-menu hyperlink");
+    hyperlink.set_inner_html(&"A");
+    
+    wrapper.append_child(&toolbar); // 에디터내에 툴바 div를 추가.
     wrapper.append_child(&editor); // 추가
+    
     Ok(()) //리턴
 }
-
-// use wasm_bindgen::prelude::*;
-
-// #[wasm_bindgen]
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
