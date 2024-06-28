7fecdf03d5a6c20f3df16176656bcaf5a4d04d83
Marhead <clgns0415@gmail.com>
2024-06-28 16:38:17 +0900

    delete comment code

--- src/lib.rs
+++ src/lib.rs
@@ -53,10 +53,3 @@ fn markdown_to_html(markdown: &str) -> String {
     html::push_html(&mut html_output, parser);
     html_output
 }
-
-// pub fn markdown_to_html(markdown: &str) -> String {
-//     let parser = Parser::new(markdown);
-//     let mut html_output = String::new();
-//     html::push_html(&mut html_output, parser);
-//     html_output
-// }
